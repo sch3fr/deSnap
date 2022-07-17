a9e26c7b2b55dd05efe059a11fe3c1384f892d63
sch3fr <50205729+sch3fr@users.noreply.github.com>
2022-07-17 16:29:00 +0200

    Small tweaks

--- deSnap/Program.cs
+++ deSnap/Program.cs
@@ -14,7 +14,7 @@
             DirectoryInfo taskDirectory = new DirectoryInfo(path);
             FileInfo[] taskFiles = taskDirectory.GetFiles("PGTA5*");  //looks for files that contain the PGTA5* string, adds them to an array
             int stop = taskFiles.Length; //finds the lenght of taskFiles array for later usage
-            int counter = 0; //counter for moving throuhg the files and naming purposes
+            int counter = 0; //counter for moving through the files and naming purposes
 
             string desktop = Environment.GetFolderPath(Environment.SpecialFolder.Desktop) + @"\deSnap"; //desktop path
             DirectoryInfo dir = Directory.CreateDirectory(desktop); //creates the deSnap folder on Desktop
@@ -30,8 +30,9 @@
                 System.IO.File.WriteAllBytes(desktop + @"\deSnap" + fileName, output); //acuall file outputs
                 counter++;
             }
-            Console.WriteLine("Sucessfuly converted {0} images!. Your images are in a folder on your Desktop", counter);
+            Console.WriteLine("Sucessfuly converted {0} images! Your images are in a folder on your Desktop", counter);
             Console.WriteLine("Press any key to exit.");
+            Console.ReadKey();
         }
     }
 }
